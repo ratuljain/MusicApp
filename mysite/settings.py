a82280203eb505c9977c1cc5c0cbf8e76516a92f
Ratul <ratul_jain1991@gmail.com>
2016-07-07 16:33:56 +0530

    Changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
-import graphlab
 
-loaded_model = graphlab.load_model('/Users/lol/Desktop/Machine Learning/personalized_model')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
